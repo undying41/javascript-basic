f2787221e602a35552f4b392143f560956b89298
undying41 <undying4@gmail.com>
2024-01-07 10:06:31 +0100

    Poznanie zmiennych
    dodano nowa funkcje

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-let a = 1;
-console.log(a);
-a = "test";
-console.log(a);
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+//Funkcja wyższego poziomu
+function calculate(a, b, fn) {
+  console.log(fn.name);
+  const res = fn(a, b);
+  return res;
+}
+
+const res = calculate(3, 5, add);
+console.log(res);
