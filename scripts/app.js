12e5299ea80b2a9ebf888db42b018fd063aedbcd
undying41 <undying4@gmail.com>
2024-01-14 08:22:53 +0100

    finish app

--- scripts/app.js
+++ scripts/app.js
@@ -17,6 +17,10 @@ const page = {
     daysContainer: document.getElementById("days"),
     nextDay: document.querySelector(".habbit__day"),
   },
+  popup: {
+    index: document.getElementById("add-habbit-popup"),
+    iconField: document.querySelector(".popup__form input[name='icon']"),
+  },
 };
 
 /* utils  */
@@ -100,24 +104,21 @@ function render(activeHabbitId) {
 
 /* work with days */
 function addDays(event) {
-  const form = event.target;
   event.preventDefault();
-  const data = new FormData(form);
-  const comment = data.get("comment");
-  form["comment"].classList.remove("error");
-  if (!comment) {
-    form["comment"].classList.add("error");
+  const data = validateAndGetFormData(event.target, ["comment"]);
+  if (!data) {
+    return;
   }
   habbits = habbits.map((habbit) => {
     if (habbit.id === globalActiveHabbitId) {
       return {
         ...habbit,
-        days: habbit.days.concat([{ comment }]),
+        days: habbit.days.concat([{ comment: data.comment }]),
       };
     }
     return habbit;
   });
-  form["comment"].value = "";
+  resetForm(event.target, ["comment"]);
   render(globalActiveHabbitId);
   saveData();
 }
@@ -135,6 +136,76 @@ function deleteDay(index) {
   });
   render(globalActiveHabbitId);
   saveData();
+}
+
+function togglePopup() {
+  if (page.popup.index.classList.contains("cover_hidden")) {
+    page.popup.index.classList.remove("cover_hidden");
+  } else {
+    page.popup.index.classList.add("cover_hidden");
+  }
+}
+
+/* working with habbits */
+
+function setIcon(context, icon) {
+  page.popup.iconField.value = icon;
+  const activeIcon = document.querySelector(".icon.icon_active");
+  activeIcon.classList.remove("icon_active");
+  context.classList.add("icon_active");
+}
+
+function resetForm(form, fields) {
+  for (const field of fields) {
+    form[field].value = "";
+  }
+}
+
+function validateAndGetFormData(form, fields) {
+  const formData = new FormData(form);
+  const res = {};
+  for (const field of fields) {
+    const fieldValue = formData.get(field);
+    form[field].classList.remove("error");
+
+    if (!fieldValue) {
+      form[field].classList.add("error");
+    }
+    res[field] = fieldValue;
+  }
+  let isValid = true;
+  for (const field of fields) {
+    if (!isValid) {
+      isValid = false;
+    }
+  }
+  if (!isValid) {
+    return;
+  }
+  return res;
+}
+
+function addHabbit(event) {
+  event.preventDefault();
+  const data = validateAndGetFormData(event.target, ["name", "icon", "target"]);
+  if (!data) {
+    return;
+  }
+  const maxId = habbits.reduce(
+    (acc, habbit) => (acc > habbit.id ? acc : habbit.id),
+    0
+  );
+  habbits.push({
+    id: maxId + 1,
+    name: data.name,
+    target: data.target,
+    icon: data.icon,
+    days: [],
+  });
+  resetForm(event.target, ["name", "target"]);
+  togglePopup();
+  saveData();
+  render(maxId + 1);
 }
 
 /* init */
